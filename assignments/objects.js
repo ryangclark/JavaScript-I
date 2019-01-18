dbf879b37ab440f49d6965f0c50fb37aed97d3c2
Clark <rclark010@gmail.com>
2019-01-18 16:56:29 -0700

    Completed the objects.js assignment.

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,20 +19,60 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const alpha = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+const bravo = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+  speak: function(){
+    return "Hello, my name is Kennan!";
+  }
+}
+const charlie = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+const delta = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+const echo = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F",
+  multiplyNums: function(x,y){
+    return x * y;
+  }
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(alpha.name);
 
 // Kennan's ID
+console.log(bravo.id);
 
 // Keven's email
+console.log(charlie.email);
 
 // Gannie's name
+console.log(delta.name);
 
 // Antonietta's Gender
+console.log(echo.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
@@ -50,16 +90,42 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: 'Susan',
+  age: 70,
+  speak: function() {
+    return `My name is ${this.name}`;
+  },
+  child: {
+    name: 'George',
+    age: 50,
+    speak: function() {
+      return `My name is ${this.name}`;
+    },
+    grandchild: {
+      name: 'Sam',
+      age: 30,
+      speak: function() {
+        return `My name is ${this.name}`;
+      }
+    }
+  },
+}
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild.name, parent.child.grandchild.age);
 
 // Have the parent speak
+console.log(parent.speak());
 
 // Have the child speak
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
