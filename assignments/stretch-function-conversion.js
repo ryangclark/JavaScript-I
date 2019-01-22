76692cd0fa43eb23cddfc4c586b14d84166974cd
Clark <rclark010@gmail.com>
2019-01-22 10:44:29 -0700

    Fixed my arrow functions.

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -11,14 +11,18 @@ let anotherFunction = param => param;
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-let add = (param1, param2) => param1 + param2;
-console.log(add(1,2));
+let add = (param1, param2) => {
+    return param1 + param2;
+}
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-let subtract = (param1, param2) => param1 - param2;
-console.log(subtract(1,2));
+let subtract = (param1, param2) => {
+    return param1 - param2;
+}
+subtract(1,2);
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
